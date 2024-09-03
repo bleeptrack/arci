5a603cf4421ba3a7ec990b6266e7103a85d21c16
bleeptrack <mail@bleeptrack.de>
2024-09-03 12:47:57 +0200

    style fake chat

--- public/InteractionTypes/InteractionFakeChat.js
+++ public/InteractionTypes/InteractionFakeChat.js
@@ -23,59 +23,461 @@ export default class InteractionFakeChat extends HTMLElement {
 			<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 			<style>
 				
-				img{
-					max-width: 90%;
-					max-height: 50%;
-				}
-				button{<link href="${window.location.origin}/static/player-style-classes.css" rel="stylesheet" />
-					width: 20%;
-				}
-				#text{
-					flex-grow: 1;
-				}
-				#message{
-					display: flex;
-					direction: row;
-					width: 90%;
-				}
-				#chat{
+				.page {
 					width: 100%;
-					height: 100%; 
-					overflow: scroll;
+					height: 100%;
 					display: flex;
-					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					font-family: "Roboto", sans-serif;
+					}
+
+					.marvel-device .screen {
+					text-align: left;
+					}
+
+					.screen-container {
+					height: 100%;
+					}
+
+					/* Status Bar */
+
+					.status-bar {
+					height: 25px;
+					background: #004e45;
+					color: #fff;
+					font-size: 14px;
+					padding: 0 8px;
+					}
+
+					.status-bar:after {
+					content: "";
+					display: table;
+					clear: both;
+					}
+
+					.status-bar div {
+					float: right;
+					position: relative;
+					top: 50%;
+					transform: translateY(-50%);
+					margin: 0 0 0 8px;
+					font-weight: 600;
+					}
+
+					/* Chat */
+
+					.chat {
+					height: calc(100% - 69px);
+					}
+
+					.chat-container {
+					height: 100%;
+					}
+
+					/* User Bar */
+
+					.user-bar {
+					height: 55px;
+					background: #005e54;
+					color: #fff;
+					padding: 0 8px;
+					font-size: 24px;
+					position: relative;
+					z-index: 1;
+					}
+
+					.user-bar:after {
+					content: "";
+					display: table;
+					clear: both;
+					}
+
+					.user-bar div {
+					float: left;
+					transform: translateY(-50%);
+					position: relative;
+					top: 50%;
+					}
+
+					.user-bar .actions {
+					float: right;
+					margin: 0 0 0 20px;
+					}
+
+					.user-bar .actions.more {
+					margin: 0 12px 0 32px;
+					}
+
+					.user-bar .actions.attachment {
+					margin: 0 0 0 30px;
+					}
+
+					.user-bar .actions.attachment i {
+					display: block;
+					transform: rotate(-45deg);
+					}
+
+					.user-bar .avatar {
+					margin: 0 0 0 5px;
+					width: 36px;
+					height: 36px;
+					position: relative;
+					overflow: hidden;
+					border-radius: 50%;
+					}
+
+					.user-bar .avatar img {
+					display: inline;
+					margin: 0 auto;
 					
-				}
-				#content{
+					height: 100%;
+					width: 100%;
+					}
+
+					.user-bar .name {
+					font-size: 17px;
+					font-weight: 600;
+					text-overflow: ellipsis;
+					letter-spacing: 0.3px;
+					margin: 0 0 0 8px;
+					overflow: hidden;
+					white-space: nowrap;
+					width: 110px;
+					}
+
+					.user-bar .status {
+					display: block;
+					font-size: 13px;
+					font-weight: 400;
+					letter-spacing: 0;
+					}
+
+					/* Conversation */
+
+					.conversation {
+					height: calc(100% - 12px);
+					position: relative;
+					background: #efe7dd url("https://cloud.githubusercontent.com/assets/398893/15136779/4e765036-1639-11e6-9201-67e728e86f39.jpg") repeat;
+					z-index: 0;
+					}
+
+					.conversation ::-webkit-scrollbar {
+					transition: all .5s;
+					width: 5px;
+					height: 1px;
+					z-index: 10;
+					}
+
+					.conversation ::-webkit-scrollbar-track {
+					background: transparent;
+					}
+
+					.conversation ::-webkit-scrollbar-thumb {
+					background: #b3ada7;
+					}
+
+					.conversation .conversation-container {
+					height: calc(100% - 68px);
+					box-shadow: inset 0 10px 10px -10px #000000;
+					overflow-x: hidden;
+					padding: 0 16px;
+					margin-bottom: 5px;
+					}
+
+					.conversation .conversation-container:after {
+					content: "";
+					display: table;
+					clear: both;
+					}
+
+					/* Messages */
+
+					.message {
+					color: #000;
+					clear: both;
+					line-height: 18px;
+					font-size: 15px;
+					padding: 8px;
+					position: relative;
+					margin: 8px 0;
+					max-width: 85%;
+					word-wrap: break-word;
+					z-index: -1;
+					}
+
+					.message:after {
+					position: absolute;
+					content: "";
+					width: 0;
+					height: 0;
+					border-style: solid;
+					}
+
+					.metadata {
+					display: inline-block;
+					float: right;
+					padding: 0 0 0 7px;
+					position: relative;
+					bottom: -4px;
+					}
+
+					.metadata .time {
+					color: rgba(0, 0, 0, .45);
+					font-size: 11px;
+					display: inline-block;
+					}
+
+					.metadata .tick {
+					display: inline-block;
+					margin-left: 2px;
+					position: relative;
+					top: 4px;
+					height: 16px;
+					width: 16px;
+					}
+
+					.metadata .tick svg {
+					position: absolute;
+					transition: .5s ease-in-out;
+					}
+
+					.metadata .tick svg:first-child {
+					-webkit-backface-visibility: hidden;
+							backface-visibility: hidden;
+					-webkit-transform: perspective(800px) rotateY(180deg);
+							transform: perspective(800px) rotateY(180deg);
+					}
+
+					.metadata .tick svg:last-child {
+					-webkit-backface-visibility: hidden;
+							backface-visibility: hidden;
+					-webkit-transform: perspective(800px) rotateY(0deg);
+							transform: perspective(800px) rotateY(0deg);
+					}
+
+					.metadata .tick-animation svg:first-child {
+					-webkit-transform: perspective(800px) rotateY(0);
+							transform: perspective(800px) rotateY(0);
+					}
+
+					.metadata .tick-animation svg:last-child {
+					-webkit-transform: perspective(800px) rotateY(-179.9deg);
+							transform: perspective(800px) rotateY(-179.9deg);
+					}
+
+					.message:first-child {
+					margin: 16px 0 8px;
+					}
+
+					.message.received {
+					background: #fff;
+					border-radius: 0px 5px 5px 5px;
+					float: left;
+					}
+
+					.message.received .metadata {
+					padding: 0 0 0 16px;
+					}
+
+					.message.received:after {
+					border-width: 0px 10px 10px 0;
+					border-color: transparent #fff transparent transparent;
+					top: 0;
+					left: -10px;
+					}
+
+					.message.sent {
+					background: #e1ffc7;
+					border-radius: 5px 0px 5px 5px;
+					float: right;
+					}
+
+					.message.sent:after {
+					border-width: 0px 0 10px 10px;
+					border-color: transparent transparent transparent #e1ffc7;
+					top: 0;
+					right: -10px;
+					}
+
+					/* Compose */
+
+					.conversation-compose {
 					display: flex;
-					flex-direction: column;
+					flex-direction: row;
+					align-items: flex-end;
 					justify-content: space-between;
-				}
-				.bubble{
+					gap: 10px;
+					padding: 0 10px;
+					height: 50px;
+					width: calc(100% - 20px);
+					z-index: 2;
+					}
+
+					.conversation-compose div,
+					.conversation-compose input {
+					background: #fff;
+					height: 100%;
+					}
+
+					.conversation-compose .emoji {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					background: white;
 					
-					max-width: 70%;
+					flex: 0 0 auto;
 					
-					padding: 0.5em;
-					margin-bottom: 1em;
-					border-radius: var(--button-border-radius);
-					font-family: sans-serif;
-					align-content: center;
-				}
-				.own{
-					align-self: end;
-					background-color: white;
-				}
-				.other{
-					align-self: start;
-					background-color: lightgrey;
-				}
-				.typing{
-					opacity: 0.5;
-				}
+					width: 48px;
+					}
+
+					.conversation-compose .input-msg {
+					border: 0;
+					border-radius: 5px 5px 5px 5px;
+					
+					flex: 1 1 auto;
+					font-size: 16px;
+					position: relative;
+					outline: none;
+					min-width: 50%;
+					padding-left: 10px;
+					}
+
+					.conversation-compose .photo {
+					flex: 0 0 auto;
+					border-radius: 0 0 5px 0;
+					text-align: center;
+					position: relative;
+					width: 48px;
+					}
+
+					
+
+					.conversation-compose .photo i {
+					display: block;
+					color: #7d8488;
+					font-size: 24px;
+					transform: translate(-50%, -50%);
+					position: relative;
+					top: 50%;
+					left: 50%;
+					}
+
+					.conversation-compose .send {
+					background: #008a7c;
+					border-radius: 50%;
+					color: #fff;
+					
+					width: 48px;
+					height: 48px;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					padding: 0;
+					border: none;
+					}
+
+					.send svg{
+						width: 50%;
+						height: 50%;
+					}
+
+					.conversation-compose .send .circle {
+					
+					}
+
+					
+
+					/* Small Screens */
+
+					@media (max-width: 768px) {
+						.marvel-device.nexus5 {
+							border-radius: 0;
+							flex: none;
+							padding: 0;
+							max-width: none;
+							overflow: hidden;
+							height: 100%;
+							width: 100%;
+						}
+
+						.marvel-device > .screen .chat {
+							visibility: visible;
+						}
+
+						.marvel-device {
+							visibility: hidden;
+						}
+
+						.marvel-device .status-bar {
+							display: none;
+						}
+
+						.screen-container {
+							position: absolute;
+							top: 0;
+							left: 0;
+							right: 0;
+							bottom: 0;
+						}
+
+						.conversation {
+							height: calc(100svh - 55px);
+						}
+						.conversation .conversation-container {
+							height: calc(100svh - 120px);
+						}
+					}	
 			</style>
 			<div id="content">
-				<h1>${msg.text}</h1>
-				<div id="chat"></div>
+				
+				<div class="page">
+				<div class="marvel-device nexus5">
+					<div class="screen">
+					<div class="screen-container">
+						
+						<div class="chat">
+						<div class="chat-container">
+							<div class="user-bar">
+							<div class="back">
+								<i class="zmdi zmdi-arrow-left"></i>
+							</div>
+							<div class="avatar">
+								<img src="./media/${this.info.avatar}" alt="Avatar">
+							</div>
+							<div class="name">
+								<span>${this.info.personName}</span>
+								<span class="status">online</span>
+							</div>
+							<div class="actions more">
+								<i class="zmdi zmdi-more-vert"></i>
+							</div>
+							<div class="actions attachment">
+								<i class="zmdi zmdi-attachment-alt"></i>
+							</div>
+							<div class="actions">
+								<i class="zmdi zmdi-phone"></i>
+							</div>
+							</div>
+							<div class="conversation">
+							<div class="conversation-container" id="chat-content">
+								
+							</div>
+							<div class="conversation-compose">
+								
+								<input class="input-msg" id="input" name="input" placeholder="Type a message" autocomplete="off" autofocus></input>
+								
+								<button class="send" id="send">
+									<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
+								</button>
+							</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+				</div>
+				</div>
 			</div>
 		`;
 
@@ -83,12 +485,30 @@ export default class InteractionFakeChat extends HTMLElement {
 		this.shadow.appendChild(container.content.cloneNode(true));
 		callback({status: "ok"})
 		
+		this.shadow.getElementById("send").addEventListener("click", () => {
+			this.addSpeechBubble(this.shadow.getElementById("input").value, true)
+			this.shadow.getElementById("input").value = ""
+		})
 		
+		/*
+		 * <h1>${msg.text}</h1>
+				<div id="chat"></div>
+				*/
+		
+		
+		//</div>
+		//<div class="message received">
+		//You were drunk.
+		//<span class="metadata"><span class="time"></span></span>
+		//</div>
+		
+		//<span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>
 		
 		
 	}
 
 	addSpeechBubble(text, own){
+		/*
 		let foundTyping = this.shadow.getElementById("chat").querySelector(".typing")
 		
 		if(foundTyping && !own){
@@ -114,7 +534,19 @@ export default class InteractionFakeChat extends HTMLElement {
 				}
 			}
 		}
-		this.shadow.getElementById("chat").scrollTop = this.shadow.getElementById("chat").scrollHeight;
+		*/
+		let tickSVG = `<span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>`
+		let type = own ? "sent" : 'received'
+		let ticks = own ? tickSVG : ""
+		let bubble = `
+			</div>
+			<div class="message ${type}">
+			${text}
+			<span class="metadata"><span class="time"></span>${ticks}</span>
+			</div>
+		`
+		this.shadow.getElementById("chat-content").innerHTML += bubble
+		this.shadow.getElementById("chat-content").scrollTop = this.shadow.getElementById("chat-content").scrollHeight;
 	}
 	
 	static handleAnswer(header, container, msg){
@@ -161,7 +593,13 @@ export default class InteractionFakeChat extends HTMLElement {
 			container.appendChild(controlContent.content.cloneNode(true));
 			header.setAttribute("cueID", msg.info.id)
 			container.querySelector("#messagecount").innerHTML = 1
-			let msgText = `${msg.info.items[0]['message']}</br>${msg.info.items[0]['message-de']}</br>${msg.info.items[0]['message-th']}`
+			let msgText = `${msg.info.items[0]['message']}`
+			if(msg.info.items[0]['message-de']){
+				msgText += `</br>${msg.info.items[0]['message-de']}`
+			}
+			if(msg.info.items[0]['message-th']){
+				msgText += `</br>${msg.info.items[0]['message-th']}`
+			}
 			container.querySelector("#message").innerHTML = msgText
 			container.querySelector("#message").setAttribute("owner", msg.info.items[0]["own-message"])
 			container.querySelector("#sendbtn").addEventListener("click", () => {
@@ -170,7 +608,13 @@ export default class InteractionFakeChat extends HTMLElement {
 				container.dispatchEvent(new CustomEvent("interaction:show-update", {detail: {text:text, own:own}}))
 				let currentid = Number( container.querySelector("#messagecount").innerHTML )
 				if(msg.info.items[currentid]){
-					msgText = `${msg.info.items[currentid]['message']}</br>${msg.info.items[currentid]['message-de']}</br>${msg.info.items[currentid]['message-th']}`
+					msgText = `${msg.info.items[currentid]['message']}`
+					if(msg.info.items[0]['message-de']){
+						msgText += `</br>${msg.info.items[0]['message-de']}`
+					}
+					if(msg.info.items[0]['message-th']){
+						msgText += `</br>${msg.info.items[0]['message-th']}`
+					}
 					container.querySelector("#message").innerHTML = msgText
 					container.querySelector("#message").setAttribute("owner", msg.info.items[currentid]["own-message"])
 					container.querySelector("#messagecount").innerHTML = currentid + 1
@@ -205,7 +649,8 @@ export default class InteractionFakeChat extends HTMLElement {
 	}
 	
 	static createFields(form){
-		CustomInput.textInput(form, "text", "Title:")
+		CustomInput.textInput(form, "personName", "Contact Name:")
+		CustomInput.filepicker(form, "avatar", "Contact Photo:")
 		let list = document.createElement("ul")
 		list.setAttribute("name", "items")
 		
